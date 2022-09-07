dc53a23899ab5c6bc2504e19ebb17ac0c853cdbe
Miyuyaku <noasaramiyu@gmail.com>
2022-09-07 13:26:44 +0000

    set validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,8 +37,12 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+            $request->validate([
+             'title' => 'required',
+             'content' => 'required',
+         ]);
     
-       $post = new Post();
+        $post = new Post();
         $post->title = $request->input('title');
         $post->content = $request->input('content');
         $post->save();
@@ -77,6 +81,10 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+             $request->validate([
+             'title' => 'required',
+             'content' => 'required',
+         ]);
         $post->title = $request->input('title');
         $post->content = $request->input('content');
         $post->save();
